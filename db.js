a437a98891116ffafcc2b36758b568da391e818a
Sarthak <kdamsarthak96k@gmail.com>
2024-11-14 15:45:03 +0530

    Fix db.js configuration with SSL settings

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-postgresql://taskM.db_owner:BbDvClQ43OLA@ep-calm-thunder-a5kfnaw1.us-east-2.aws.neon.tech/taskM.db?sslmode=require
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  
+  },
 });
 
 module.exports = pool;
